cfbfaac7c79e1a8e8aedeb43d71291f24f1f4de9
BulSV <SB.ua@yandex.ru>
2015-07-01 11:48:07 +0300

    Initial commit

---
+++ init.c
@@ -0,0 +1,81 @@
+/////////////////////////////////////
+//  Generated Initialization File  //
+/////////////////////////////////////
+
+#include "compiler_defs.h"
+#include "C8051F410_defs.h"
+
+// Peripheral specific initialization functions,
+// Called from the Init_Device() function
+void Timer_Init()
+{
+    TMOD      = 0x20;
+    CKCON     = 0x08;
+    TH1       = 0x96;
+}
+
+void SPI_Init()
+{
+    SPI0CFG   = 0x40;
+    SPI0CN    = 0x01;
+    SPI0CKR   = 0x0B;
+}
+
+void Port_IO_Init()
+{
+    // P0.0  -  Skipped,     Push-Pull,  Digital
+    // P0.1  -  Skipped,     Push-Pull,  Digital
+    // P0.2  -  Skipped,     Open-Drain, Digital
+    // P0.3  -  Skipped,     Open-Drain, Digital
+    // P0.4  -  TX0 (UART0), Push-Pull,  Digital
+    // P0.5  -  RX0 (UART0), Open-Drain, Digital
+    // P0.6  -  Skipped,     Open-Drain, Digital
+    // P0.7  -  Skipped,     Push-Pull,  Digital
+
+    // P1.0  -  Skipped,     Open-Drain, Digital
+    // P1.1  -  Skipped,     Open-Drain, Digital
+    // P1.2  -  Skipped,     Open-Drain, Digital
+    // P1.3  -  Skipped,     Open-Drain, Digital
+    // P1.4  -  SCK  (SPI0), Push-Pull,  Digital
+    // P1.5  -  MISO (SPI0), Open-Drain, Digital
+    // P1.6  -  MOSI (SPI0), Push-Pull,  Digital
+    // P1.7  -  Unassigned,  Open-Drain, Digital
+
+    // P2.0  -  Unassigned,  Push-Pull,  Digital
+    // P2.1  -  Unassigned,  Push-Pull,  Digital
+    // P2.2  -  Unassigned,  Push-Pull,  Digital
+    // P2.3  -  Unassigned,  Push-Pull,  Digital
+    // P2.4  -  Unassigned,  Push-Pull,  Digital
+    // P2.5  -  Unassigned,  Push-Pull,  Digital
+    // P2.6  -  Unassigned,  Push-Pull,  Digital
+    // P2.7  -  Unassigned,  Open-Drain, Digital
+
+    P0MDOUT   = 0x93;
+    P1MDOUT   = 0x50;
+    P2MDOUT   = 0x7F;
+    P0SKIP    = 0xCF;
+    P1SKIP    = 0x0F;
+    XBR0      = 0x03;
+    XBR1      = 0x40;
+}
+
+void Oscillator_Init()
+{
+    OSCICN    = 0x87;
+}
+
+void Interrupts_Init()
+{
+    IE        = 0x50;
+}
+
+// Initialization function for device,
+// Call Init_Device() from your main program
+void Init_Device(void)
+{
+    Timer_Init();
+    SPI_Init();
+    Port_IO_Init();
+    Oscillator_Init();
+    Interrupts_Init();
+}
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <c8051f410.h>
+#include <stdio.h>
+
+extern void Init_Device(void);
+
+void main(void)
+{
+	Init_Device();
+	
+	while(1)
+	{
+		
+	}
+}
